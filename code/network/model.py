8d564172d7f0e27dc1d1fbd63a0011f19a1ef923
Arpit Aggarwal <arpitdec5@gmail.com>
2020-10-31 21:42:42 -0400

    Worked on the implementation of deeplabv3 and deeplabv3+, modified files in code/ folder

---
+++ code/network/model.py
@@ -0,0 +1,78 @@
+# reference: https://github.com/VainF/DeepLabV3Plus-Pytorch/blob/master/network/modeling.py
+# header files
+from .utils import IntermediateLayerGetter
+from ._deeplab import DeepLabHead, DeepLabHeadV3Plus, DeepLabV3
+from .backbone import resnet
+
+
+def _segm_resnet(name, backbone_name, num_classes, output_stride, arma):
+    if output_stride==8:
+        replace_stride_with_dilation=[False, True, True]
+        aspp_dilate = [12, 24, 36]
+    else:
+        replace_stride_with_dilation=[False, False, True]
+        aspp_dilate = [6, 12, 18]
+
+    backbone = resnet.__dict__[backbone_name](replace_stride_with_dilation=replace_stride_with_dilation, arma=arma)
+    inplanes = 2048
+    low_level_planes = 256
+
+    if name=='deeplabv3plus':
+        return_layers = {'layer4': 'out', 'layer1': 'low_level'}
+        classifier = DeepLabHeadV3Plus(inplanes, low_level_planes, num_classes, aspp_dilate, arma=arma)
+    elif name=='deeplabv3':
+        return_layers = {'layer4': 'out'}
+        classifier = DeepLabHead(inplanes , num_classes, aspp_dilate, arma=arma)
+    backbone = IntermediateLayerGetter(backbone, return_layers=return_layers)
+
+    model = DeepLabV3(backbone, classifier)
+    return model
+
+def _load_model(arch_type, backbone, num_classes, output_stride, arma):
+    if backbone=='mobilenetv2':
+        model = _segm_mobilenet(arch_type, backbone, num_classes, output_stride=output_stride, arma=arma)
+    elif backbone.startswith('resnet'):
+        model = _segm_resnet(arch_type, backbone, num_classes, output_stride=output_stride, arma=arma)
+    else:
+        raise NotImplementedError
+    return model
+
+
+# Deeplab v3
+def deeplabv3_resnet50(num_classes=19, output_stride=8, arma=False):
+    """Constructs a DeepLabV3 model with a ResNet-50 backbone.
+    Args:
+        num_classes (int): number of classes.
+        output_stride (int): output stride for deeplab.
+        arma: boolean value
+    """
+    return _load_model('deeplabv3', 'resnet50', num_classes, output_stride=output_stride, arma=arma)
+
+def deeplabv3_resnet101(num_classes=19, output_stride=8, arma=False):
+    """Constructs a DeepLabV3 model with a ResNet-101 backbone.
+    Args:
+        num_classes (int): number of classes.
+        output_stride (int): output stride for deeplab.
+        arma: boolean value
+    """
+    return _load_model('deeplabv3', 'resnet101', num_classes, output_stride=output_stride, arma=arma)
+
+
+# Deeplab v3+
+def deeplabv3plus_resnet50(num_classes=19, output_stride=8, arma=False):
+    """Constructs a DeepLabV3 model with a ResNet-50 backbone.
+    Args:
+        num_classes (int): number of classes.
+        output_stride (int): output stride for deeplab.
+        arma: boolean value
+    """
+    return _load_model('deeplabv3plus', 'resnet50', num_classes, output_stride=output_stride, arma=arma)
+
+def deeplabv3plus_resnet101(num_classes=19, output_stride=8, arma=False):
+    """Constructs a DeepLabV3+ model with a ResNet-101 backbone.
+    Args:
+        num_classes (int): number of classes.
+        output_stride (int): output stride for deeplab.
+        arma: boolean value
+    """
+    return _load_model('deeplabv3plus', 'resnet101', num_classes, output_stride=output_stride, arma=arma)
